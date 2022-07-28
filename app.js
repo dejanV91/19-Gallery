17ee04c92565c090126ac76f44a72c0e08d5b73f
dejanV91 <dejanvukoicic91@gmail.com>
2022-07-28 22:15:11 +0200

    initial commit

--- app.js
+++ app.js
@@ -13,12 +13,15 @@ class Nature{
 
         this.modal = getElement(".modal");
         this.closeBtn = getElement(".close-btn");
-        this.closeBtn = getElement(".close-btn");
         this.previousBtn = getElement(".back");
         this.nextBtn = getElement(".forward");
         this.mainImage = getElement(".image-content");
         this.imageTitle = getElement(".titleMain");
         this.imagesListModal = getElement(".images-list");
+
+        this.closeModal = this.closeModal.bind(this);
+        this.nextImage = this.nextImage.bind(this);
+        this.previousImage = this.previousImage.bind(this);
 
         this.container.addEventListener("click", function(e){
             if (e.target.classList.contains("img")) {
@@ -34,12 +37,14 @@ class Nature{
                 return `<img 
                 src="${item.src}" 
                 alt="${item.alt}" 
-                title="${item.title} 
-                id="${item.dataset.id}
+                title="${item.title}"
+                id="${item.dataset.id}"
                 class="${selectedItem.dataset.id === item.dataset.id ? "imageM selected" : "imageM"}">`;
             }).join("");
             this.modal.classList.add("open");
-            this.closeBtn.addEventListener("click", closeModal);
+            this.closeBtn.addEventListener("click", this.closeModal);
+            this.nextBtn.addEventListener("click", this.nextImage);
+            this.previousBtn.addEventListener("click", this.previousImage);
         }
 
         setMainImage(selectedItem){
@@ -47,8 +52,26 @@ class Nature{
             this.imageTitle.textContent = selectedItem.title;
         }
 
-        
+        closeModal(){
+            this.modal.classList.remove("open");
+        }
 
+        nextImage(){
+            const current = this.imagesListModal.querySelector(".selected");
+            const next = current.nextSibling || this.imagesListModal.firstChild;
+
+            current.classList.remove("selected");
+            next.classList.add("selected");
+            this.setMainImage(next);
+        }
+
+        previousImage(){
+            const current = this.imagesListModal.querySelector(".selected");
+            const prev = current.previousSibling || this.imagesListModal.lastElementChild;
+            current.classList.remove("selected");
+            prev.classList.add("selected");
+            this.setMainImage(prev);
+        }
     }
 
 const elem = new Nature(getElement(".images-nature"));
